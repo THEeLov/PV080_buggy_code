cd710b50c4031fe4e98f5bb4b2ee2f8b3b728553
Filip Kozik <536410@mail.muni.cz>
2024-04-16 15:01:47 +0200

    codeql

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -2,13 +2,12 @@ import sys
 import os
 import yaml
 import flask
-from typing import Dict, Any, Optional, Union
 
 app = flask.Flask(__name__)
 
 
 @app.route("/")
-def index() -> Optional[str]:
+def index():
     version = flask.request.args.get("urllib_version")
     url = flask.request.args.get("url")
     return fetch_website(version, url)
@@ -22,11 +21,11 @@ class Person(object):
         self.name = name
 
 
-def print_nametag(format_string: str, person: Person) -> None:
+def print_nametag(format_string, person):
     print(format_string.format(person=person))
 
 
-def fetch_website(urllib_version: Optional[str], url: str) -> None:
+def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
@@ -38,13 +37,13 @@ def fetch_website(urllib_version: Optional[str], url: str) -> None:
         print('Exception')
 
 
-def load_yaml(filename: str) -> Any:
+def load_yaml(filename):
     stream = open(filename)
-    deserialized_data = yaml.load(stream, Loader=yaml.Loader)  # deserializing data
+    deserialized_data = yaml.safe_load(stream, Loader=yaml.Loader)  # deserializing data
     return deserialized_data
 
 
-def authenticate(password: str) -> None:
+def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
@@ -56,16 +55,16 @@ if __name__ == '__main__':
     print("2. Code injection vulnerability:")
     print("3. Yaml deserialization vulnerability:")
     print("4. Use of assert statements vulnerability:")
-    choice = input("Select vulnerability: ")
-    if choice == "1":
+    CHOICE = input("Select vulnerability: ")
+    if CHOICE == "1":
         new_person = Person("Vickie")
         print_nametag(input("Please format your nametag: "), new_person)
-    elif choice == "2":
+    elif CHOICE == "2":
         urlib_version = input("Choose version of urllib: ")
         fetch_website(urlib_version, url="https://www.google.com")
-    elif choice == "3":
+    elif CHOICE == "3":
         load_yaml(input("File name: "))
         print("Executed -ls on current folder")
-    elif choice == "4":
+    elif CHOICE == "4":
         password = input("Enter master password: ")
         authenticate(password)
